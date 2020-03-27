1a65d1a02f015754f307e5ec80f3e036ce6e162e
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-03-27 00:27:07 -0400

    Updates

---
+++ precache-manifest.1050cca9aa8271273e52316e1644d421.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a078695f31ec2b6082a025d39786c7",
+    "url": "/covid-visualizer/index.html"
+  },
+  {
+    "revision": "351175de7e7c4a7ad68e",
+    "url": "/covid-visualizer/static/css/2.2c3d7176.chunk.css"
+  },
+  {
+    "revision": "a389bfa0db08d2360023",
+    "url": "/covid-visualizer/static/css/main.150bf3b5.chunk.css"
+  },
+  {
+    "revision": "351175de7e7c4a7ad68e",
+    "url": "/covid-visualizer/static/js/2.cfab309b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-visualizer/static/js/2.cfab309b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a389bfa0db08d2360023",
+    "url": "/covid-visualizer/static/js/main.dba032ad.chunk.js"
+  },
+  {
+    "revision": "da673638ed6b3e7d4039",
+    "url": "/covid-visualizer/static/js/runtime-main.ff6204c3.js"
+  }
+]);
