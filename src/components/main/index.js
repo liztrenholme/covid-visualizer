c5baee27513348e305b6d0953ee8f4425bf26c41
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-04-05 01:18:46 -0400

    changed to stateMode or nationalMode, added state selector, ability to view counties of any state, color tweaks, clear out viz before new render

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -6,69 +6,116 @@ import Network from '../network';
 class Main extends Component {
   state = {
     data: {},
-    ohioMode: true
+    stateMode: true,
+    nationalMode: false,
+    selectedState: 'Ohio',
+    availableStates: []
   };
 
   async componentDidMount() {
     const data = await getData('USA');
     if (data && data.statusCode === 200) {
-      this.setState({data: data.data});
+      const availableStates = [];
+      data.data.covid19Stats.forEach(i => availableStates.push(i.province));
+      this.setState({data: data.data, availableStates});
     }
   }
-  toggleMode = () => this.state.ohioMode ? this.setState({ohioMode: false}) : this.setState({ohioMode: true});
+  clearViz = (e) => this.setState({stateMode: false, nationalMode: false, selectedState: e.currentTarget.value}, () => {
+    this.chooseState();
+  });
+  chooseState = () => {
+    this.setState({stateMode: true});
+  }
+  toggleMode = () => this.state.stateMode 
+    ? this.setState({stateMode: false, nationalMode: true}) 
+    : this.setState({stateMode: true, nationalMode: false});
   render() {
-    const { data, ohioMode } = this.state;
-    const ohioStats = data && data.covid19Stats && data.covid19Stats.length ? data.covid19Stats.filter(i => i.province === 'Ohio') : [];
-    const ohioNodes = ohioStats.length 
-      ? ohioStats.map(i => {return({ id: i.city, label: `${i.city} ${i.confirmed}`, shape: 'circle', color: i.confirmed > 5000 ? '#964eba' : i.confirmed > 1000 ? '#ba4e66' : i.confirmed > 500 ? '#f00' : i.confirmed > 100 ? 'orange' : i.confirmed > 50 ? '#FFFF00' : i.confirmed === 0 ? '#fff' : '#fcfbd9' });}).concat([{id: 'Ohio', label: 'Ohio'}]) : [{id: 'Ohio', label: 'Ohio'}];
-    const ohioEdges = ohioStats.length ? ohioStats.map(i => {return({ from: i.province, to: i.city });}) : [];
+    const { data, nationalMode, stateMode, selectedState } = this.state;
+    const stateStats = data && data.covid19Stats && data.covid19Stats.length 
+      ? data.covid19Stats.filter(i => i.province === selectedState) : [];
+    const stateNodes = stateStats.length 
+      ? stateStats.map(i => {return({ 
+        id: `${i.city} city`, 
+        label: `${i.city} ${i.confirmed}`, 
+        shape: 'circle',
+        color: i.confirmed > 5000 ? '#964eba' 
+          : i.confirmed > 1000 ? '#ba4e66'
+            : i.confirmed > 500 ? '#f00' 
+              : i.confirmed > 100 ? 'orange' 
+                : i.confirmed > 50 ? '#FFFF00' 
+                  : i.confirmed === 0 ? '#fff' 
+                    : '#fcfbd9' });}).concat([{id: selectedState, label: selectedState}]) : []; //[{id: selectedState, label: selectedState}];
+    const stateEdges = stateStats.length ? stateStats.map(i => {return({ from: i.province, to: `${i.city} city` });}) : [];
 
-    const states = data && data.covid19Stats && data.covid19Stats.length ? [...new Set(data.covid19Stats.map(i => {return(i.province);}))] : [];
+    const states = data && data.covid19Stats && data.covid19Stats.length 
+      ? [...new Set(data.covid19Stats.map(i => {return(i.province);}))] : [];
+    states.sort();
     const statesArray = states.map(i => {return({id: i, label: i});});
     const allNodes = data && data.covid19Stats && data.covid19Stats.length 
-      ? data.covid19Stats.map(i => {return({ id: i.keyId, label: `${i.city} ${i.confirmed}`, shape: 'circle', color: i.confirmed > 5000 ? '#964eba' : i.confirmed > 1000 ? '#ba4e66' : i.confirmed > 500 ? '#f00' : i.confirmed > 100 ? 'orange' : i.confirmed > 50 ? '#FFFF00' : i.confirmed === 0 ? '#fff' : '#fcfbd9' });}).concat(statesArray) : [{id: 'Ohio', label: 'Ohio'}];
+      ? data.covid19Stats.map(i => {return({ 
+        id: `${i.keyId} city`, 
+        label: `${i.city} ${i.confirmed}`, 
+        shape: 'circle', 
+        color: i.confirmed > 5000 ? '#964eba' 
+          : i.confirmed > 1000 ? '#ba4e66' 
+            : i.confirmed > 500 ? '#f00' 
+              : i.confirmed > 100 ? 'orange' 
+                : i.confirmed > 50 ? '#FFFF00' 
+                  : i.confirmed === 0 ? '#fff' 
+                    : '#fcfbd9' });}).concat(statesArray) : []; // [{id: 'Ohio', label: 'Ohio'}];
     const allEdges = data && data.covid19Stats && data.covid19Stats.length 
-      ? data.covid19Stats.map(i => {return({ from: i.province, to: i.keyId });}) : [];
+      ? data.covid19Stats.map(i => {return({ from: i.province, to: `${i.keyId} city` });}) : [];
     const dateUpdated = data && data.lastChecked 
       ? `${data.lastChecked.split('T')[0]}, ${data.lastChecked.split('T')[1].split('.')[0]}` : '';
     return (
       <div className="main">
         <h1>Covid-19 Stats By County</h1><span>Updated: {dateUpdated}</span>
-        <div className={ohioMode ? 'active-button button' : 'inactive-button button'}
-          onClick={this.toggleMode}>
-                Ohio
-        </div>
-        <div className={ohioMode ? 'inactive-button button' : 'active-button button'}
-          onClick={this.toggleMode}>
+        <div className='button-container'>
+          <div className={stateMode ? 'active-button button' : 'inactive-button button'}
+            onClick={this.toggleMode}>
+          State Mode
+          </div>
+          <select className='inactive-button' onChange={this.clearViz} value={selectedState}>
+            {states && states.length ? states.map(i => {
+              return(
+                <option 
+                  key={i} 
+                  label={i}
+                  value={i}>{i}</option>
+              );}) : null}
+          </select>
+          <div className={nationalMode ? 'active-button button' : 'inactive-button button'}
+            onClick={this.toggleMode}>
                 All States
+          </div>
         </div>
         <div className="main-layout">
           <div className="counties-container">
-            <h2>Ohio Counties</h2>
+            <h2>{selectedState} Counties</h2>
             <ul className="county-list">
-              {ohioStats.length ? 
-                ohioStats.map(i => {return(<li key={i.city}>
+              {stateStats.length ? 
+                stateStats.map(i => {return(<li key={i.city}>
                   <p className="counties" style={i.confirmed > 50 ? {color: 'red', fontWeight: 'bold'} : {}}>
-                    {i.city}: {i.confirmed}
+                    {i.city || i.province}: {i.confirmed}
                   </p>
                 </li>);}) : null}
             </ul>
           </div>
           <div className="visualizer">
             {data && data.covid19Stats && data.covid19Stats.length
-            && ohioMode
+            && stateMode && !nationalMode
               ? <Network 
                 data={data}
-                nodes={ohioNodes}
-                edges={ohioEdges}
-                ohioMode={ohioMode} /> : null}
+                nodes={stateNodes}
+                edges={stateEdges}
+                ohioMode={stateMode} /> : null}
             {data && data.covid19Stats && data.covid19Stats.length
-            && !ohioMode
+            && nationalMode && !stateMode
               ? <Network 
                 data={data}
                 nodes={allNodes}
                 edges={allEdges}
-                ohioMode={ohioMode} /> : null}
+                ohioMode={stateMode} /> : null}
           </div>
           {/* <div className="graph-3d">
           graph 3d
